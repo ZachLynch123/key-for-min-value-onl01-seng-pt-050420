174e5bc9020a5d236e3202c6f359ec346be01334
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-05-13 18:18:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  new_value = 500000
   name_hash.each do |name, value|
+    if value < new_value
+      new_value = value
+    end
     
 end
